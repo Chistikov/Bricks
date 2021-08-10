ca0a4c91b9030ff9055aea07051b17fb10b1f9df
Chistikov <vlad.chistikov998@gmail.com>
2021-08-10 21:17:23 +0300

    Added .prettierrc

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function MSRY(config) {
 
   this.init = function () {
     this.config = {
-      rootSelector: '#MSRY',
+      rootSelector: "#MSRY",
       settings: {
         columnsCount: 1,
       },
-      onScreenWidth: {}
+      onScreenWidth: {},
     };
 
     // todo: rewrite appending options
@@ -16,16 +16,16 @@ function MSRY(config) {
 
     this.rootElement = document.querySelector(this.config.rootSelector);
     if (!this.rootElement) {
-      throw new Error('Wrong selector. Element not found.')
+      throw new Error("Wrong selector. Element not found.");
     }
 
     this.imagesArr = document.querySelectorAll(`${this.config.rootSelector} > img`);
     this.deleteChildrenFromParent(this.rootElement);
 
-    window.addEventListener('resize', onResizeWindowHandler.bind(this));
+    window.addEventListener("resize", onResizeWindowHandler.bind(this));
     pasteColumnsAndImages.call(this);
 
-    console.log(this.imagesArr)
+    console.log(this.imagesArr);
 
     function onResizeWindowHandler() {
       this.deleteChildrenFromParent(this.rootElement);
@@ -37,20 +37,20 @@ function MSRY(config) {
       let ap = getAppropriateWidth();
 
       // counting containers
-      if (ap === 'default') {
+      if (ap === "default") {
         columnsCount = this.config.settings.columnsCount;
       } else {
         columnsCount = this.config.onScreenWidth[ap]?.columnsCount || this.config.settings.columnsCount;
       }
 
-      console.log(ap, columnsCount)
+      console.log(ap, columnsCount);
 
       // creating containers
       for (let i = 0; i < columnsCount; i++) {
-        let wrapper = document.createElement('div');
-        wrapper.classList.add('msry_wrapper__imgContainer');
+        let wrapper = document.createElement("div");
+        wrapper.classList.add("msry_wrapper__imgContainer");
         let space = `- ${(columnsCount - 1) * 10}px)`;
-        wrapper.style.width = `calc((100% ${columnsCount > 1 ? space : ""} / ${columnsCount})`
+        wrapper.style.width = `calc((100% ${columnsCount > 1 ? space : ""} / ${columnsCount})`;
         this.rootElement.appendChild(wrapper);
       }
 
@@ -58,7 +58,6 @@ function MSRY(config) {
       // let containerWithMinHeight = null;
       let pos = 0;
       Array.from(this.imagesArr).forEach((image, index) => {
-
         // this.rootElement.children[index % this.imagesArr.length].appendChild(image)
         this.rootElement.children[pos].appendChild(image);
 
@@ -71,18 +70,20 @@ function MSRY(config) {
 
     function getAppropriateWidth() {
       let windowWidth = window.innerWidth;
-      let widthArr = Object.keys(config.onScreenWidth).map(width => Number(width)).sort((a, b) => {
-        if (a < b) {
-          return 1;
-        } else {
-          return -1;
-        }
-      });
+      let widthArr = Object.keys(config.onScreenWidth)
+        .map((width) => Number(width))
+        .sort((a, b) => {
+          if (a < b) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
 
       let appropriateWidth;
       for (let i = 0; i < widthArr.length; i++) {
         if (windowWidth > widthArr[0]) {
-          appropriateWidth = 'default';
+          appropriateWidth = "default";
         } else if (windowWidth <= widthArr[i]) {
           appropriateWidth = widthArr[i];
         }
@@ -94,7 +95,7 @@ function MSRY(config) {
 
   this.deleteChildrenFromParent = function (parentElement) {
     parentElement.innerHTML = "";
-  }
+  };
 
   this.init();
 }
